7e872ebe307baf7d7ac63fb6a933d7d5ddb57b39
Jordi <ofineo@gmail.com>
2017-08-17 21:53:54 +0100

    stars v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StarComponent } from 'app/shared/star.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StarComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+  ],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
