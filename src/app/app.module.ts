a61f50fcf2713457b1cd1d6663e0efb8aac35438
Jordi <ofineo@gmail.com>
2017-09-24 21:47:49 +0100

    quotations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { StarComponent } from 'app/shared/star.component';
 import { DashBoardComponent } from 'app/Dashboard/dashBoard.Component';
+import { DefaultComponent } from './default/default.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     StarComponent,
-    DashBoardComponent
+    DashBoardComponent,
+    DefaultComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       {path: 'dashboard', component: DashBoardComponent},
-      {path: '', component: DashBoardComponent},
-      {path: '**', component: DashBoardComponent},
-    ])
+      {path: '', component: DefaultComponent},
+      {path: '**', component: DefaultComponent},
+    ]),
+    BrowserAnimationsModule
   ],
   providers: [
   ],
---
+++ src/app/default/default.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Observable, Subscription } from 'rxjs/Rx';
+import { trigger, state, animate, transition, style } from '@angular/animations';
+
+@Component({
+  selector: 'app-default',
+  templateUrl: './default.component.html',
+  styleUrls: ['./default.component.css'],
+  animations: [
+    trigger('visibilityChanged', [
+      state('true', style({ opacity: 1, transform: 'scale(1.0)' })),
+      state('false', style({ opacity: 0, transform: 'scale(0.0)' })),
+      transition('1 => 0', animate('0.5s')),
+      transition('0 => 1', animate('0.3s'))
+    ])
+  ],
+})
+export class DefaultComponent implements OnInit, OnDestroy {
+  @Input() isVisible: boolean = true;
+  // tslint:disable-next-line:max-line-length
+  timer: any;
+  sub: any;
+  // tslint:disable-next-line:max-line-length
+  quotations = [
+    'Twenty years from now you will be more disappointed by the things that you didn\'t do than by the ones you did do. So throw off the bowlines. Sail away from the safe harbor. Catch the trade winds in your sails. Explore. Dream. Discover.',
+    'Do the best you can until you know better. Then, when you know better, do better.'
+  ];
+  quotation: string;
+  i: number;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.timer = Observable.timer(2000, 5000);
+    this.sub = this.timer.subscribe(t => {
+      this.qoutesRotation();
+    });
+    this.i = Math.floor((Math.random() * 2));
+    this.quotation = this.quotations[this.i];
+  };
+
+  ngOnDestroy() {
+    console.log('Destroy timer');
+    // unsubscribe here
+    this.sub.unsubscribe();
+  };
+
+  qoutesRotation() {
+
+    this.isVisible = this.isVisible ? false : true;
+    this.i = Math.floor((Math.random() * 2));
+    this.quotation = this.quotations[this.i];
+    setTimeout(() => {
+      this.isVisible = this.isVisible ? false : true;
+    }, 500)
+  };
+}
